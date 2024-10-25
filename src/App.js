4d5f2eb8fa14f00f03c9694b613ff6c5fddf83e4
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2024-10-25 10:23:48 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarshallingDemo from './MarshallingDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <MarshallingDemo />
+    </div>
+  );
+}
+
+export default App;
